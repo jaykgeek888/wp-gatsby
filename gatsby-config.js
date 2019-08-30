7fcbed111e876a7266fe71b6b712736064ff3936
jayk_geek888 <jaysonsupsup@gmail.com>
2019-08-31 01:19:59 +0800

    configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,37 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        baseUrl: 'trygatsby.wpengine.com',
+        protocol: 'http',
+        hostingWPCOM: false,
+        useACF: true,
+        acfOptionPageIds: [],
+        verboseOutput: false,
+        perPage: 100,
+        searchAndReplacementContentUrls: {
+          sourceUrl: 'http://trygatsby.wpengine.com',
+          replacementUrl: 'http://localhost:8000',
+        },
+        concurrentRequests: 10,
+        includedRoutes: [
+          '**/categories',
+          '**/posts',
+          '**/pages',
+          '**/media',
+          '**/tags',
+          '**/taxonomies',
+          '**/users',
+        ],
+        excludedRoutes: [],
+        normalizer: function({entities}){
+          return entities
+        }
+      },
+    },
+    'gatsby-plugin-sitemap',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
